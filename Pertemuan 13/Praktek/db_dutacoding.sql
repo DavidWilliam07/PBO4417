1d47266d40b32894aa0cb87c879706a3811b1a71
David William <80263936+DavidWilliam07@users.noreply.github.com>
2021-06-15 21:38:10 +0700

    Add files via upload

---
+++ Pertemuan 13/Praktek/db_dutacoding.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2021 at 04:39 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_dutacoding`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pendaftaran`
+--
+
+CREATE TABLE `tbl_pendaftaran` (
+  `no` int(3) NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `jk` varchar(1) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `usia` int(3) NOT NULL,
+  `alasan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pendaftaran`
+--
+
+INSERT INTO `tbl_pendaftaran` (`no`, `nik`, `nama`, `jk`, `alamat`, `usia`, `alasan`) VALUES
+(1, '3374020304050001', 'David', 'L', 'Jl. Nakula 1 no.20', 45, 'Ingin menambah pengetahuan');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
